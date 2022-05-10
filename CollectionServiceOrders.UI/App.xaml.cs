a05999b54e6718ae196bdc5b0737e0871bc2f854
CrimsonOrion <crimsonorion@gmail.com>
2022-05-10 12:15:19 -0400

    Finished UI

--- CollectionServiceOrders.UI/App.xaml.cs
+++ CollectionServiceOrders.UI/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
 
 namespace CollectionServiceOrders.UI;
@@ -12,4 +7,100 @@ namespace CollectionServiceOrders.UI;
 /// </summary>
 public partial class App
 {
+    protected override Window CreateShell() => Container.Resolve<MainWindow>();
+
+    protected override void OnStartup(StartupEventArgs e) => base.OnStartup(e);//ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncAll;//ThemeManager.Current.SyncTheme();
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        // Set up the custom logger to go to Desktop
+        ICustomLogger logger = new CustomLogger(
+            new FileInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "CCEMC Collection Service Orders Errors.log")),
+            true,
+            LogLevel.Error);
+
+        // Set up the configuration using the appSettings.json file.
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json", false, true)
+            .Build();
+
+        GlobalConfig.EmailConfig = new()
+        {
+            SmtpServer = configuration["Email Settings:Smtp Server"] ?? "",
+            AdminEmail = configuration["Email Settings:Admin Email"] ?? "",
+            AdminText = configuration["Email Settings:Admin Text"] ?? ""
+        };
+
+        GlobalConfig.DepositInfoConfiguration = new()
+        {
+            MinimumDepositElectric = decimal.TryParse(configuration["Deposit Info:Minimum Deposit Electric"], out var d) ? d : 0m,
+            MinimumDepositWater = decimal.TryParse(configuration["Deposit Info:Minimum Deposit Water"], out d) ? d : 0m,
+            MinimumBillElectric = decimal.TryParse(configuration["Deposit Info:Minimum Bill Electric"], out d) ? d : 0m,
+            MinimumBillWater = decimal.TryParse(configuration["Deposit Info:Minimum Bill Water"], out d) ? d : 0m,
+            MinimumAdditionalDeposit = decimal.TryParse(configuration["Deposit Info:Minimum Additional Deposit"], out d) ? d : 0m
+        };
+
+        GlobalConfig.SignalChargeFeesConfiguration = new()
+        {
+            SignalNumbers = new()
+        };
+
+        foreach (IConfigurationSection item in configuration.GetSection("Signal Charge Fees").GetChildren())
+        {
+            SignalNumberModel signalNumber = new()
+            {
+                SignalNumber = item.Key,
+                FeeString = item.Value
+            };
+            signalNumber.Description = signalNumber.SignalNumber switch
+            {
+                "1" => $"{signalNumber.SignalNumber} - ERROR-RECON ASAP",
+                "1B" => $"{signalNumber.SignalNumber} - ERROR BKT-RECON ASAP",
+                "2" => $"{signalNumber.SignalNumber} - CUT",
+                "2B" => $"{signalNumber.SignalNumber} - BKT TRK CUT",
+                "3" => $"{signalNumber.SignalNumber} - TRIP CHARGE",
+                "3B" => $"{signalNumber.SignalNumber} - BKT TRIP",
+                "4" => $"{signalNumber.SignalNumber} - RECONNECT",
+                "4B" => $"{signalNumber.SignalNumber} - BKT RECON",
+                "OT4" => $"{signalNumber.SignalNumber} - OT RECON",
+                "OT4B" => $"{signalNumber.SignalNumber} - BTK OT RECON",
+                "7" => $"{signalNumber.SignalNumber} - KILL C/O",
+                "9" => $"{signalNumber.SignalNumber} - FIELD CONNECT",
+                "9B" => $"{signalNumber.SignalNumber} - BKT COLLECT",
+                _ => $"{signalNumber.SignalNumber} - ???"
+            };
+            GlobalConfig.SignalChargeFeesConfiguration.SignalNumbers.Add(signalNumber);
+        }
+
+        GlobalConfig.TemplateFileLocationConfiguration = new()
+        {
+            LetterheadBwDfs = configuration["Template File Locations:Letterhead-BW-DFS"] ?? "",
+            LetterheadBwHardcode = configuration["Template File Locations:Letterhead-BW-Hardcode"] ?? ""
+        };
+
+        _ = containerRegistry
+            .RegisterInstance(logger)
+
+            .RegisterInstance<IDialogCoordinator>(new DialogCoordinator())
+
+            .RegisterScoped<ISqlDataAccess, MsSqlDataAccess>()
+            .RegisterScoped<IOdbcDataAccess, OdbcDataAccess>()
+            .RegisterScoped<IEmailer, FluentEmailerMailKit>()
+            .RegisterScoped<IErrorHandler, ErrorHandler>()
+            .RegisterScoped<IDataProcessor, DataProcessor>()
+            .RegisterScoped<IWorksheet, Worksheet>()
+            .RegisterScoped<ILetter, Letter>()
+            ;
+    }
+
+    protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog) => _ = moduleCatalog
+        .AddModule<AboutModule>()
+        .AddModule<CutoffScreenModule>()
+        .AddModule<AdditionalDepositModule>()
+        .AddModule<AdhocReportModule>()
+        .AddModule<AddNewCutoffModule>()
+        .AddModule<SearchForCutoffModule>()
+        .AddModule<ImportDataFromDaffronModule>()
+        .AddModule<AdministrationModule>()
+        ;
 }
---
+++ CollectionServiceOrders.UI/GlobalUsings.cs
@@ -0,0 +1,31 @@
+﻿global using About.Module;
+
+global using AdditionalDeposit.Module;
+
+global using AddNewCutoff.Module;
+
+global using AdhocReport.Module;
+
+global using Administration.Module;
+
+global using CollectionServiceOrders.Core.Configuration;
+global using CollectionServiceOrders.Core.ErrorHandler;
+global using CollectionServiceOrders.Core.Processors;
+global using CollectionServiceOrders.Core.Reporting;
+
+global using CutoffScreen.Module;
+
+global using ImportDataFromDaffron.Module;
+
+global using Library.NET.DataAccess;
+global using Library.NET.Logging;
+global using Library.NET.Mailer;
+
+global using MahApps.Metro.Controls.Dialogs;
+
+global using Microsoft.Extensions.Configuration;
+
+global using Prism.Ioc;
+global using Prism.Modularity;
+
+global using SearchForCutoff.Module;
